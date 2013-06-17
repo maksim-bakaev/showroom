a39e9ccc7649ff51bdfd502383b577cc7e6a6746
maksim bakaev <maksim.bakaev@pokupon.ua>
2013-06-18 00:28:04 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showroom::Application.config.secret_token = '3541f73f79e08cefc65bf748b6adcb8e113246433cbc265128c6de3ef7c2b6a34fc79327c4303544086bab255f1af5978754e35dfa3293e98721f468974630ad'
