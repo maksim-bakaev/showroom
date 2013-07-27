0669b4cb3bef50b491f2e0cdd61c56d101c90406
maxmax <maxkv333@gmail.com>
2013-07-27 23:23:40 +0300

    architect fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,14 @@
 class DashboardController < ApplicationController
-  
+
   def index
   end
   
   def landing
+    render layout: "landing"
   end
   
   def show
+    render layout: "custom"
   end
 
 end
